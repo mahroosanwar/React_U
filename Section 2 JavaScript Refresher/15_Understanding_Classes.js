690b04082ab22c9fa99fe2ef5267b005c018b40e
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-09-02 23:14:27 +0530

    Create 15_Understanding_Classes.js

---
+++ Section 2 JavaScript Refresher/15_Understanding_Classes.js
@@ -0,0 +1,24 @@
+class Human {
+  constructor() {
+    this.gender = "Male"
+  }
+  
+  myGender() {
+    console.log(this.gender)
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super();
+    this.name = "Mahroos"
+  }
+  
+  myName() {
+      (console.log(name))
+  }
+}
+
+const person = new Person()
+person.myName()
+person.myGender()
